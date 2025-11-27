e0bbe35cdbaa1267f9207eb9894cf877c304f249
Caio Gonçalves <128558158+caiGon1@users.noreply.github.com>
2025-11-26 21:35:40 -0300

    atualização de texto e integração sis mesas a grafica

--- lanchoneteListas/src/lanchonetelistas/LanchoneteListas.java
+++ lanchoneteListas/src/lanchonetelistas/LanchoneteListas.java
@@ -1,6 +1,12 @@
 package lanchonetelistas;
 public class LanchoneteListas {
     public static void main(String[] args) {
+          javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new interfacegrafica().setVisible(true);
+            }
+        });
     }
     
 }
